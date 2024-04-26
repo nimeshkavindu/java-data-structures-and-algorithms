0b0e056739099dd2734c333e4a356e08694269e3
Nimesh Kavindu <124415338+nimeshkavindu@users.noreply.github.com>
2024-04-26 09:53:20 +0530

    Create Print_Using_For_Loop.java
    Create java program to print an array using for loop

---
+++ Code_Examples/Print_Using_For_Loop.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Using a for loop to print elements
+        System.out.println("Using for loop:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
+    }
+}
